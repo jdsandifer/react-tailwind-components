c756c9afa00e4c139fb54d4b0ffcd46e3a34a16a
J.D. Sandifer <jd.sandifer@adpearance.com>
2021-02-03 14:16:51 -0800

    Add TailwindCSS, confirm it works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="text-center">
+            <header className="bg-gray-800 min-h-screen flex flex-col align-center justify-center text-3xl text-white">
+                <img
+                    src={logo}
+                    className="pointer-events-none h-96 App-logo"
+                    alt="logo"
+                />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="text-blue-300"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
