425cfb4a4ca5bce4d4c87d8531e604de9d8a470a
J.D. Sandifer <jd.sandifer@adpearance.com>
2021-02-04 00:18:48 -0800

    Add more data

--- src/pages/BudgetTable.js
+++ src/pages/BudgetTable.js
@@ -30,6 +30,124 @@ const data = [
         deltaPercent: -1068,
         oneDayDeltaPercent: -1343,
     },
+    {
+        code: '08fe7138',
+        name: 'Shannon',
+        google: 49881,
+        bing: 64207,
+        total: 159763,
+        budget: 78728,
+        pacing: 484312,
+        delta: -3416,
+        deltaPercent: -3472,
+        oneDayDeltaPercent: -8327,
+    },
+    {
+        name: 'Randolph',
+        google: 286354,
+        bing: 137511,
+        total: 166048,
+        budget: 78728,
+        pacing: 341405,
+        delta: '-3128',
+        deltaPercent: '-3194',
+        oneDayDeltaPercent: '-7488',
+    },
+    {
+        code: 'eb92d5c5',
+        name: 'Daniels',
+        google: 193157,
+        bing: 14753,
+        total: 229636,
+        budget: 78728,
+        pacing: 129308,
+        delta: '-3658',
+        deltaPercent: '-2637',
+        oneDayDeltaPercent: '-4066',
+    },
+    {
+        code: '601babcc',
+        name: 'Dalton',
+        google: 177768,
+        bing: 10137,
+        total: 235325,
+        budget: 78728,
+        pacing: 233525,
+        delta: '-3805',
+        deltaPercent: '-2362',
+        oneDayDeltaPercent: '-3484',
+    },
+    {
+        code: '601bacdc',
+        name: 'Sexton',
+        google: 272232,
+        bing: 260238,
+        total: 141666,
+        budget: 78728,
+        pacing: 427312,
+        delta: '-1665',
+        deltaPercent: '-2953',
+        oneDayDeltaPercent: '-3498',
+    },
+    {
+        code: 'bc5a1ce3',
+        name: 'Morris',
+        google: 250213,
+        bing: 299232,
+        total: 101841,
+        budget: 78728,
+        pacing: 759301,
+        delta: '-2330',
+        deltaPercent: '-2790',
+        oneDayDeltaPercent: '-9617',
+    },
+    {
+        name: 'Warner',
+        google: 113747,
+        bing: 132772,
+        total: 350247,
+        budget: 78728,
+        pacing: 220924,
+        delta: '-2192',
+        deltaPercent: '-2170',
+        oneDayDeltaPercent: '-5233',
+    },
+    {
+        code: '601baba4',
+        name: 'Diaz',
+        google: 173555,
+        bing: 249054,
+        total: 126014,
+        budget: 78728,
+        pacing: 341694,
+        delta: '-1039',
+        deltaPercent: '-3683',
+        oneDayDeltaPercent: '-4281',
+    },
+    {
+        code: '601babc6',
+        name: 'Dennis',
+        google: 204205,
+        bing: 245818,
+        total: 238935,
+        budget: 78728,
+        pacing: 388341,
+        delta: '-9365',
+        deltaPercent: '-2263',
+        oneDayDeltaPercent: '-4781',
+    },
+    {
+        code: '601bf49f',
+        name: 'Gonzales',
+        google: 141838,
+        bing: 285513,
+        total: 391603,
+        budget: 78728,
+        pacing: 221380,
+        delta: '-8455',
+        deltaPercent: '-5734',
+        oneDayDeltaPercent: '-5491',
+    },
 ]
 
 const formatDollars = (amountInCents) =>
@@ -120,9 +238,6 @@ const BudgetTable = () => (
                                 >
                                     Scrub
                                 </th>
-                                <th scope="col" className="relative px-6 py-3">
-                                    <span className="sr-only">Edit</span>
-                                </th>
                             </tr>
                         </thead>
                         <tbody className="bg-white divide-y divide-gray-200">
@@ -188,14 +303,6 @@ const BudgetTable = () => (
                                         <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
                                             No scrub
                                         </span>
-                                    </td>
-                                    <td className="px-6 py-6 whitespace-nowrap text-right text-sm font-medium">
-                                        <a
-                                            href="/"
-                                            className="text-indigo-600 hover:text-indigo-900"
-                                        >
-                                            Edit
-                                        </a>
                                     </td>
                                 </tr>
                             ))}
